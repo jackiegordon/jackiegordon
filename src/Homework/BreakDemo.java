5bd0a1363b69dbb7e6d86b99a5481c5958418525
jackietorres <jackietorres@jacquelines-air.fios-router.home>
2022-01-04 16:00:48 -0500

    Homework 8

--- src/Homework/BreakDemo.java
+++ src/Homework/BreakDemo.java
@@ -2,4 +2,30 @@ package Homework;
 
 public class BreakDemo {
 
+	public static void main(String[] args) {
+		
+		for (int i =0; i<= 15; i++) {
+		
+			if (i==7) {
+				System.out.println("Dania's favorite Number: " +i);
+				break;
+			}
+		System.out.println(i);
+		
+		
+	}
 }
+}
+/**
+
+break keyword
+
+--> used inside switch statement 
+
+--> purpose: breaks out from switch statement
+
+
+
+--> Break Keyword can be also used inside LOOP BODY -> it TERMINATES/EXITS/STOPS the loop
+
+*/
--- src/Homework/homeworkq5.java
+++ src/Homework/homeworkq5.java
@@ -2,4 +2,25 @@ package Homework;
 
 public class homeworkq5 {
 
+	public static void main(String[] args) {
+		
+		int evenTotal = 0;
+		
+		int oddTotal = 0;
+		
+		int count = 1;
+		
+		while (count <=20) {
+			if (count % 2 ==0)
+				evenTotal = evenTotal + count;
+			
+		} else {
+			oddTotal = oddTotal + count;
+		}
+		count++;
+		
+	}
+	System.out.println("While Even Total: " + Even Total);
+	System.out.println("While Odd Total: " + Odd Total);
+
 }
